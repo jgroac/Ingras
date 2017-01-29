42c626c65d265764dbf57ea195c03d32b7040c31
Jose Roa <jg.roac@gmail.com>
2017-01-29 17:36:34 -0400

    refactored userActions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,9 +1,18 @@
-import { AUTHENTICATE_USER } from '../constants/actionTypes';
+import { AUTHENTICATE_USER, DEAUTHENTICATE_USER } from '../constants/actionTypes';
+import auth from '../utils/auth';
 
 export const authenticate = (token) => {
+  auth.authenticateUser(token);
   return {
     type: AUTHENTICATE_USER,
-    token
+
+  };
+};
+
+export const logout = (token) => {
+  auth.deauthenticateUser(token);
+  return {
+    type: DEAUTHENTICATE_USER,
   };
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import userReducer from './user';
 import profileReducer from './profile';
 import postsReducer from './posts';
 
 
 const rootReducer = combineReducers({
   routing: routerReducer,
-  user: userReducer,
   profile: profileReducer,
   posts: postsReducer
 });
