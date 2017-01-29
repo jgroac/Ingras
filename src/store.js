52f442f5628a813cf86b43cc6c6732416c61a0c0
Jose Roa <jg.roac@gmail.com>
2017-01-29 00:05:37 -0400

    compose middleware & enhancers

--- src/store.js
+++ src/store.js
@@ -1,17 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createLogger from 'redux-logger';
 import rootReducer from './reducers';
 
 
 const logger = createLogger();
-
 /* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(logger))
 );
-/* eslint-enable */
 
 store.dispatch({type: 'ADD_TODO', todo: {}});
 
