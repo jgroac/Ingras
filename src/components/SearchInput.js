3f42e3e427e661302917d4467da68b4247711afc
Jose Roa <jg.roac@gmail.com>
2017-01-28 20:30:45 -0400

    fixed style issue

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchInput = (props) => {
   return (
     <div className="search-input mt-2" style={{marginLeft: 'auto', marginRigth: 'auto'}}>
-      <input className="pl-4" type="text" id="search" placeholder="Search" />
+      <input className="pl-4 pr-1" type="text" id="search" placeholder="Search" />
       <label className="search-input__placeholder pt-1 m-0" htmlFor="search">
         <span>
           <i className="sprite-icon sprite-search d-inline-block m-0">icon</i> Search
